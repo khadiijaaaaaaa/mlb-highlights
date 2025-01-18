7929c333eb7634fed03a6e31cbe2aee1a014706c
Khadija Oubaha <khadijaoubaha17@gmail.com>
2025-01-18 21:37:23 +0100

    updated app.py file

--- backend/app.py
+++ backend/app.py
@@ -1,18 +1,29 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS  # Import CORS
 import pandas as pd
 
 app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes
 
-# Load the cleaned CSV data
-data = pd.read_csv('../datasets/mlb_homeruns_2024.csv')
+# Load the updated CSV data with the 'team' column
+data = pd.read_csv('../datasets/mlb_homeruns_teams_added_2024.csv')
 
 @app.route('/highlights', methods=['GET'])
 def get_highlights():
     player_name = request.args.get('player')  # Get player name from query parameter
+    team_name = request.args.get('team')  # Get team name from query parameter
+
     if player_name:
+        # Filter highlights by player name
         highlights = data[data['title'].str.contains(player_name)].to_dict(orient='records')
-        return jsonify(highlights)
-    return jsonify([])
+    elif team_name:
+        # Filter highlights by team name
+        highlights = data[data['team'].str.contains(team_name, case=False)].to_dict(orient='records')
+    else:
+        # If no player or team is specified, return all highlights
+        highlights = data.to_dict(orient='records')
+
+    return jsonify(highlights)
 
 if __name__ == '__main__':
     app.run(debug=True)
